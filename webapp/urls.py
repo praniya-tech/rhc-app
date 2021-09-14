64cb6a2058aeba4c0b9e35573eae1285f630cbeb
rasayu <rasayu.rcc@gmail.com>
2021-09-14 15:47:17 +0530

    Add favicon

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    path(
+        'favicon.ico',
+        RedirectView.as_view(url=staticfiles_storage.url('favicon.png'))
+    ),
     path('', TemplateView.as_view(template_name='webapp/index.html')),
 ]
