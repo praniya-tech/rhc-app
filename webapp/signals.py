d0101e5c088a09598e9fd365ff76f720a392ef61
rasayu <rasayu.rcc@gmail.com>
2021-09-25 09:43:09 +0530

    TEST Create CRF patient on user login
    Test logging errors.

---
+++ webapp/signals.py
@@ -0,0 +1,21 @@
+from django.dispatch import receiver
+import requests
+import logging
+
+from allauth.account.signals import user_signed_up, user_logged_in
+
+
+log = logging.getLogger('django.request')
+
+
+@receiver(user_logged_in)
+def on_user_signed_up(sender, **kwargs):
+    """Create CRF lead for newly signed up user."""
+    try:
+        user = kwargs['user']
+        response = requests.post(
+            url='http://localhost:8000/rhcapi/patient',
+            data={'email': user.email})
+        response.raise_for_status()
+    except Exception as err:
+        log.error(str(err), exc_info=err)
