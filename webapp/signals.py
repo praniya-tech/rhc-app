4d793f5df687524e955cf97462f5f3c7964c8d5d
rasayu <rasayu.rcc@gmail.com>
2021-09-25 09:49:36 +0530

    Test django logging

--- webapp/signals.py
+++ webapp/signals.py
@@ -11,11 +11,11 @@ log = logging.getLogger('__name__')
 @receiver(user_logged_in)
 def on_user_signed_up(sender, **kwargs):
     """Create CRF lead for newly signed up user."""
-    try:
-        user = kwargs['user']
-        response = requests.post(
-            url='http://localhost:8000/rhcapi/patient',
-            data={'email': user.email})
-        response.raise_for_status()
-    except Exception as err:
-        log.error(str(err), exc_info=err)
+    # try:
+    user = kwargs['user']
+    response = requests.post(
+        url='http://localhost:8000/rhcapi/patient',
+        data={'email': user.email})
+    response.raise_for_status()
+    # except Exception as err:
+    #     log.error(str(err), exc_info=err)
