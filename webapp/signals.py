f0327ba2160a2713a2af38711b51c7d8f4c350e2
rasayu <rasayu.rcc@gmail.com>
2021-09-25 09:54:20 +0530

    Test django logging

--- webapp/signals.py
+++ webapp/signals.py
@@ -5,17 +5,17 @@ import logging
 from allauth.account.signals import user_signed_up, user_logged_in
 
 
-log = logging.getLogger('__name__')
+log = logging.getLogger('django')
 
 
 @receiver(user_logged_in)
 def on_user_signed_up(sender, **kwargs):
     """Create CRF lead for newly signed up user."""
-    # try:
-    user = kwargs['user']
-    response = requests.post(
-        url='http://localhost:8000/rhcapi/patient',
-        data={'email': user.email})
-    response.raise_for_status()
-    # except Exception as err:
-    #     log.error(str(err), exc_info=err)
+    try:
+        user = kwargs['user']
+        response = requests.post(
+            url='http://localhost:8000/rhcapi/patient',
+            data={'email': user.email})
+        response.raise_for_status()
+    except Exception as err:
+        log.error(str(err), exc_info=err)
